8739af00e08556ea339834e27daf13ccabefbc5a
thecooperrollers <thecooperrollers@gmail.com>
2025-09-01 17:15:01 +0530

    Add PDF generator placeholder

---
+++ includes/includes/includes/includes/includes/includes/class-crq-pdf.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class Crq_Pdf {
+    public static function generate_pdf( $data ) {
+        // Placeholder: Here you can integrate FPDF/TCPDF for real PDFs.
+        // For now, we just return true.
+        return true;
+    }
+}
